c341e86ef8e2f40e26be3199958e9dd57b4988b1
Manish Shinde <manishshinde1415@gmail.com>
2026-05-29 17:09:38 +0530

    ApiError Class added for handling errors gracefully

---
+++ src/utility/ApiError.ts
@@ -0,0 +1,13 @@
+class ApiError extends Error {
+    statusCode : number;
+    message: string;
+    constructor(statusCode : number, message : string) {
+        super(message);
+
+        this.statusCode = statusCode;
+        this.message = message;
+        Error.captureStackTrace(this, this.constructor);
+    }
+}
+
+export default ApiError;
